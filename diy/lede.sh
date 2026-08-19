63ff14d706bfdf987a5b10d8c7b3aa2003afe3b3
forseti <63159639+forseticn@users.noreply.github.com>
2026-08-19 19:56:01 +0800

    Update lede.sh

--- diy/lede.sh
+++ diy/lede.sh
@@ -3,6 +3,3 @@ sed -i 's/192.168.1.1/10.0.0.3/g' package/base-files/files/bin/config_generate
 
 # 清除登陆密码
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
-
-./scripts/feeds update -a
-./scripts/feeds install -a
